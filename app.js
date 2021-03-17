ff84885808daadbad113ecb2692cc91c64b7dfad
ndvssankar <sivashankar@msitprogram.net>
2021-03-17 22:00:54 +0530

    updated

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! Welcome to DevOps using AWS Beanstalk'));
+app.get('/', (req, res) => res.send('Hello World! Welcome to DevOps using AWS Beanstalk.....After review'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
